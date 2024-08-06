00147be1d28bb1a499280353b6a496a08cd172da
Dongju Yeon <dongju1497@gmail.com>
2024-08-07 00:21:52 +0900

    4.5 create Posts module
    - Generate Posts module using the following command:
      ```bash
      nest g resource Posts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,26 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
-
-interface Post {
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: 'newjeans minji',
-      content: 'dancing minji',
-      likeCount: 100000000,
-      commentCount: 9999999,
-    };
-  }
 }
