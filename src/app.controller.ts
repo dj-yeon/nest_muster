c27d0611c1d3e5280a3486c89112a63c2d107d4e
Dongju Yeon <dongju1497@gmail.com>
2024-08-07 00:11:50 +0900

    4.3 return POST-JSON

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author: 'newjeans_official',
+      title: 'newjeans minji',
+      content: 'dancing minji',
+      likeCount: 100000000,
+      commentCount: 9999999,
+    };
   }
 }
