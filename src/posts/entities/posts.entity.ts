607a5a2ac8c5ea88dd33f314e002c809fb545cb4
Dongju Yeon <dongju1497@gmail.com>
2024-08-07 12:31:54 +0900

    9.6 'User Module' generated, 'Author Relation, generated

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,12 +1,15 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { UsersModel } from 'src/users/entities/users.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class PostsModel {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  author: string;
+  // 1. argument: 어떤 릴레이션과 관계?
+  // 2. argument: 어떤 property를 연동?
+  @ManyToOne(() => UsersModel, (user) => user.posts, { nullable: false })
+  author: UsersModel;
 
   @Column()
   title: string;
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersModel } from './entities/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UsersModel)
+    private readonly userRepository: Repository<UsersModel>,
+  ) {}
+
+  async createUser(nickname: string, email: string, password: string) {
+    const user = this.userRepository.create({
+      nickname,
+      email,
+      password,
+    });
+
+    const newUser = await this.userRepository.save(user);
+
+    return newUser;
+  }
+
+  async getAllUsers() {
+    return this.userRepository.find();
+  }
+}
