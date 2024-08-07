e6f79d4643333ae3b06ba44e8659f2dc53a69e13
Dongju Yeon <dongju1497@gmail.com>
2024-08-07 12:43:23 +0900

    11.9 'relation' included queries used

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -20,11 +20,16 @@ export class PostsService {
   ) {}
 
   async getAllPosts() {
-    this.postsRepository.find();
+    this.postsRepository.find({
+      relations: ['author'],
+    });
   }
 
   async getPostById(id: number) {
-    const post = await this.postsRepository.findOne({ where: { id } });
+    const post = await this.postsRepository.findOne({
+      where: { id },
+      relations: ['author'],
+    });
 
     if (!post) {
       throw new NotFoundException();
