d1b2bce1dd9bec96bafba5314b18e61f178d3f2c
Dongju Yeon <dongju1497@gmail.com>
2024-08-09 01:36:14 +0900

    15. BaseModel applied
    id, updatedAt, createdAt columns in baseModel file

--- src/posts/entities/posts.entity.ts
+++ src/posts/entities/posts.entity.ts
@@ -1,11 +1,9 @@
+import { BaseModel } from 'src/common/entity/base.entity';
 import { UsersModel } from 'src/users/entities/users.entity';
-import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne } from 'typeorm';
 
 @Entity()
-export class PostsModel {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class PostsModel extends BaseModel {
   // 1. argument: 어떤 릴레이션과 관계?
   // 2. argument: 어떤 property를 연동?
   @ManyToOne(() => UsersModel, (user) => user.posts, { nullable: false })
