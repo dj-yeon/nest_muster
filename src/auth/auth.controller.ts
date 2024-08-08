c958699762ed23e9b263365e505a2dab9da9d6c0
Dongju Yeon <dongju1497@gmail.com>
2024-08-08 18:22:21 +0900

    13. jwt basic, bearer token generated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,17 +1,47 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Headers, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Post('token/access')
+  postTokenAccess(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, false);
+
+    return {
+      accessToken: newToken,
+    };
+  }
+
+  @Post('token/refresh')
+  postTokenRefresh(@Headers('authorization') rawToken: string) {
+    const token = this.authService.extractTokenFromHeader(rawToken, true);
+
+    const newToken = this.authService.rotateToken(token, true);
+
+    return {
+      refreshToken: newToken,
+    };
+  }
+
   @Post('login/email')
-  loginEmail(@Body('email') email: string, @Body('password') password: string) {
-    return this.authService.loginWithEmail({ email, password });
+  postLoginEmail(@Headers('authorization') rawToken: string) {
+    //email:password -> base64
+    // 풀어서 -> email:password로 만들어야함
+    // 클라이언트에서 서버로 보낼 때, id,pwd 형태가 아닌,
+    // 'Basic {email:password를 base64로 인코딩한 형태}'로 보냄
+    const token = this.authService.extractTokenFromHeader(rawToken, false);
+
+    const credentials = this.authService.decodeBasicToken(token);
+
+    return this.authService.loginWithEmail(credentials);
   }
 
   @Post('regiter/email')
-  registerEmail(
+  postRegisterEmail(
     @Body('nickname') nickname: string,
     @Body('email') email: string,
     @Body('password') password: string,
