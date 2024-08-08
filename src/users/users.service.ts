85c320bb17f197e102e406f53a9db8da4e27b02a
Dongju Yeon <dongju1497@gmail.com>
2024-08-08 16:50:53 +0900

    13.11 'registerWithEmail function' generated

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersModel } from './entities/users.entity';
 import { Repository } from 'typeorm';
@@ -10,14 +10,35 @@ export class UsersService {
     private readonly userRepository: Repository<UsersModel>,
   ) {}
 
-  async createUser(nickname: string, email: string, password: string) {
-    const user = this.userRepository.create({
-      nickname,
-      email,
-      password,
+  async createUser(user: Pick<UsersModel, 'nickname' | 'email' | 'password'>) {
+    // 1) nickname 중복이 없는지 확인
+    const nicknameExists = await this.userRepository.exist({
+      where: {
+        nickname: user.nickname,
+      },
     });
 
-    const newUser = await this.userRepository.save(user);
+    if (nicknameExists) {
+      throw new BadRequestException('이미 존재하는 nickname 입니다!');
+    }
+
+    const emailExists = await this.userRepository.exist({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (emailExists) {
+      throw new BadRequestException('이미 존재하는 email 입니다!');
+    }
+
+    const userObject = this.userRepository.create({
+      nickname: user.nickname,
+      email: user.email,
+      password: user.password,
+    });
+
+    const newUser = await this.userRepository.save(userObject);
 
     return newUser;
   }
