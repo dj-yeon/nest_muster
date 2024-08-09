907fb42b5ae79d693282305b9a19b8b1418969d0
Dongju Yeon <dongju1497@gmail.com>
2024-08-09 19:13:16 +0900

    20.7 Length Annotation, Email Annotaion

--- src/users/entities/users.entity.ts
+++ src/users/entities/users.entity.ts
@@ -2,6 +2,7 @@ import { Column, Entity, OneToMany } from 'typeorm';
 import { RolesEnum } from '../const/roles.const';
 import { PostsModel } from 'src/posts/entities/posts.entity';
 import { BaseModel } from 'src/common/entity/base.entity';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 @Entity()
 export class UsersModel extends BaseModel {
@@ -9,14 +10,22 @@ export class UsersModel extends BaseModel {
     length: 20,
     unique: true,
   })
+  @IsString()
+  @Length(1, 20, {
+    message: '닉네임은 1-20자 사이',
+  })
   nickname: string;
 
   @Column({
     unique: true,
   })
+  @IsString()
+  @IsEmail()
   email: string;
 
   @Column()
+  @IsString()
+  @Length(3, 8)
   password: string;
 
   @Column({
