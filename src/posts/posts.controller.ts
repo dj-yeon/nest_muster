7911a081654c5968408cca3a98c1484f241bddcd
Dongju Yeon <dongju1497@gmail.com>
2024-08-12 02:36:33 +0900

    30.2 Interceptor를 이용해서 로거 구현

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -10,6 +10,7 @@ import {
   Post,
   Query,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AccessTokenGuard } from 'src/auth/guard/bearer-token.guard';
@@ -21,6 +22,7 @@ import { PaginatePostDto } from './dto/paginte-post.dto';
 import { ImageModelType } from 'src/common/entity/image.entity';
 import { DataSource } from 'typeorm';
 import { PostsImagesService } from './image/images.service';
+import { LogInterceptor } from 'src/common/interceptor/log.interceptor';
 
 @Controller('posts')
 export class PostsController {
@@ -31,6 +33,7 @@ export class PostsController {
   ) {}
 
   @Get()
+  @UseInterceptors(LogInterceptor)
   getPosts(@Query() query: PaginatePostDto) {
     return this.postsService.paginatePosts(query);
   }
