c755d342b6b00e0bf6d324790d38ce1b40ad5744
Dongju Yeon <dongju1497@gmail.com>
2024-08-13 01:07:21 +0900

    35.3 Paginate Comments API 만들기

--- src/posts/comments/comments.service.ts
+++ src/posts/comments/comments.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommonService } from 'src/common/common.service';
+import { CommentsModel } from './entity/comments.entity';
+import { Repository } from 'typeorm';
+import { PaginateCommentsDto } from './dto/paginate-comments.dto';
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  constructor(
+    @InjectRepository(CommentsModel)
+    private readonly commentsRepository: Repository<CommentsModel>,
+    private readonly commonService: CommonService,
+  ) {}
+  PaginateComments(dto: PaginateCommentsDto, postId: number) {
+    return this.commonService.paginate(
+      dto,
+      this.commentsRepository,
+      {
+        where: {
+          post: {
+            id: postId,
+          },
+        },
+      },
+      `posts/${postId}/comments`,
+    );
+  }
+}
